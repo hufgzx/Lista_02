e7e99e2b5bdce6aec483a334e73b93ea21b1a5ec
Gabi Duartte <66841994+hufgzx@users.noreply.github.com>
2021-09-14 14:58:55 -0300

    Update q02_numero_menor_e_maior.js

--- q02_numero_menor_e_maior.js
+++ q02_numero_menor_e_maior.js
@@ -1,22 +1,16 @@
 const prompt = require('prompt-sync')()
-console.log('o resultade é', resultado)
 
 function main(){
-    console.log('menor valor:', numero_menor)
-    console.log('maior valor:', numero_maior)
-const numero_1 = Number(prompt('numero 1: '))
-const numero_2 = Number(prompt('numero 2: '))
-
+ 
+const numero_1 = Number(prompt(' Digite o valor do numero 1: '))
+const numero_2 = Number(prompt('Digite o valor do numero 2: '))
 const numero_menor = numero_maior > numero_menor 
-const numero_maior = numero_1
-const numero_menor = numero_2
-}
+const numero_maior = numero_menor < numero_maior
 
-function resultado(valor){
-    if(numero_maior > numero_menor){
-        return true
-    } else{
-        return false
-    }
+    if(numero_1 > numero_2){
+        console.log('n1 e o maior)
+    } else if(numero_2 > numero_1){
+        console.log('n2 e maior )
+    }                 
 }
 main()
